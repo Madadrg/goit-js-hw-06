95b6bd48551cfd4d17eb82321f0ed732e2fca6d4
Georgiana-Mădălina Dragomir <dragomirgeorgianamadalina@gmail.com>
2024-01-07 17:30:13 +0200

    Worked on last exercises

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // Get the form elements using the elements property
+  const formElements = event.target.elements;
+
+  if (!formElements.email.value || !formElements.password.value) {
+    alert("All fields must be filled.");
+  } else {
+    const formData = {
+      email: formElements.email.value,
+      password: formElements.password.value,
+    };
+
+    console.log(formData);
+
+    // Reset the form fields
+    loginForm.reset();
+  }
+});
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,21 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
 }
+
+const colorSpan = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+
+function updateBackgroundColor() {
+  const randomColor = getRandomHexColor();
+
+  // Update the body background color
+  document.body.style.backgroundColor = randomColor;
+
+  // Display the color value in the span
+  colorSpan.textContent = randomColor;
+}
+
+// Add click event listener to the button
+changeColorButton.addEventListener("click", updateBackgroundColor);
