2f68b37deb9845e5704987ba5b367695aac452d0
Georgiana-Mădălina Dragomir <dragomirgeorgianamadalina@gmail.com>
2024-01-07 18:27:55 +0200

    Corrected first exercise

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,20 +1,23 @@
-// Get the ul#categories element
-var categoriesList = document.getElementById("categories");
+document.addEventListener("DOMContentLoaded", function () {
+  var categoriesList = document.getElementById("categories");
 
-// Get all the li.item elements within ul#categories
-var categoryItems = categoriesList.getElementsByClassName("item");
+  // Count and display the total number of categories
+  var categoriesCount = categoriesList.getElementsByClassName("item").length;
+  console.log("Number of categories: " + categoriesCount);
 
-console.log("Number of categories:", categoryItems.length);
+  // Iterate through each li.item element
+  var categoryItems = categoriesList.getElementsByClassName("item");
+  for (var i = 0; i < categoryItems.length; i++) {
+    var categoryItem = categoryItems[i];
 
-for (var i = 0; i < categoryItems.length; i++) {
-  var categoryItem = categoryItems[i];
+    // Find the title (h2) of the category
+    var categoryTitle = categoryItem.querySelector("h2").textContent;
 
-  // Get the title of the category (h2 tag)
-  var categoryTitle = categoryItem.querySelector("h2").textContent;
+    // Find and count the number of items in the category (li elements)
+    var categoryElementsCount = categoryItem.querySelectorAll("ul li").length;
 
-  // Get all the li elements within the current category
-  var categoryElements = categoryItem.querySelectorAll("ul li");
-
-  console.log("\nCategory:", categoryTitle);
-  console.log("Elements:", categoryElements.length);
-}
+    // Display the results in the console
+    console.log("Category: " + categoryTitle);
+    console.log("Elements: " + categoryElementsCount);
+  }
+});
